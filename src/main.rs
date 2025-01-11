994256846ad574072ce333f43ab7ec28af389206
Adem Efe Devrez <adevrez77@gmail.com>
2025-01-11 22:23:15 +0300

    Add calculation per generation function

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,18 @@ fn main() {
     let mutation_constant = 0.01; //percent
     let population = generate_initial_gen(population_count);
     let population_fitness = determine_fitness_of_gen(&population);
+    let generation_count = 100;
     for (elem, elem_fitness) in &population_fitness {
-        println!("{elem} : {elem_fitness}")
+        println!("{:?} : {elem_fitness}", split_bstring_to_two(conv_int_to_bstring(*elem)));
     }
 
-    let mut new_generation = Vec::with_capacity(population_count);
-    for _ in 0..5 {
-        let (first, second) = pick_candidates_for_breeding(&population_fitness);
-        
-        let (first_child, second_child) = breed(first, second, crossover_constant, mutation_constant);
-        new_generation.push(first_child);
-        new_generation.push(second_child);
-        println!("{first} {second} => {first_child} {second_child}");
+    let mut new_generation;
+
+    for i in 0..generation_count {
+        println!("Generation {i}");
+        new_generation = generate_new_generation(&population, population_count, crossover_constant, mutation_constant);
     }
+
 }
 
 fn fitness(s: i64) -> f64 {
@@ -31,7 +30,9 @@ fn fitness(s: i64) -> f64 {
     let x: i64 = conv_bstring_to_int(&s[0..8]);
     let y: i64 = conv_bstring_to_int(&s[8..16]);
 
-    let mut fitness_level: f64 = (((1-x)^2) as f64) * E.powi((-(x^2) - (y+1)^2) as i32) - ((x - x^3 - y^3) as f64) * E.powi((-(x^2) -(y^2)) as i32);
+    //let mut fitness_level: f64 = (((1-x)^2) as f64) * E.powi((-(x^2) - (y+1)^2) as i32) - ((x - x^3 - y^3) as f64) * E.powi((-(x^2) -(y^2)) as i32);
+
+    let mut fitness_level: f64 = (10000 - x^2 + y^2) as f64;
 
     if fitness_level < 0.0 {fitness_level = 0.0;}
     fitness_level
@@ -95,7 +96,7 @@ fn breed(first: i64, second: i64, crossover_constant: f64, mutation_constant: f6
     let mut second_child= second;
 
     if rng.gen_range(0.0..1.0) < mutation_constant {
-        let pivot = rng.gen_range(0..15);
+        let pivot = rng.gen_range(0..31);
         let mutated_char = match &binary_couple.chars().nth(pivot).unwrap() {
             '0' => "1",
             '1' => "0",
@@ -105,13 +106,36 @@ fn breed(first: i64, second: i64, crossover_constant: f64, mutation_constant: f6
     }
 
     if rng.gen_range(0.0..1.0) < crossover_constant {
-        let pivot = rng.gen_range(0..16);
+        let pivot = rng.gen_range(0..32);
         let part_one = &binary_couple[0..pivot];
-        let part_two = &binary_couple[pivot..16];
+        let part_two = &binary_couple[pivot..32];
         first_child = conv_bstring_to_int(format!("{part_one}{part_two}"));
         second_child = conv_bstring_to_int(format!("{part_two}{part_one}"));
     }
 
+    println!("{:?} , {:?} => {:?} {:?}", split_bstring_to_two(binary_first), split_bstring_to_two(binary_second), split_bstring_to_two(conv_int_to_bstring(first_child)), split_bstring_to_two(conv_int_to_bstring(second_child)));
     
     (first_child, second_child)
+}
+
+fn generate_new_generation(population: &Vec<i64>, population_count: usize, crossover_constant: f64, mutation_constant: f64) -> Vec<i64> {
+    let old_population_with_fitness = determine_fitness_of_gen(population);
+    let mut new_generation = Vec::new();
+
+    for _ in 0..population_count/2 {
+        let (first, second) = pick_candidates_for_breeding(&old_population_with_fitness);
+        
+        let (first_child, second_child) = breed(first, second, crossover_constant, mutation_constant);
+
+        new_generation.push(first_child);
+        new_generation.push(second_child);
+    }
+
+    new_generation
+}
+
+fn split_bstring_to_two(s: String) -> (i64, i64) {
+    let x: i64 = conv_bstring_to_int(&s[0..8]);
+    let y: i64 = conv_bstring_to_int(&s[8..16]);
+    (x,y)
 }
